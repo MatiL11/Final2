6e3f01329105c91d8127955352dcb418b605ddde
Mati Ps <matiaslisandro9@gmail.com>
2024-03-02 10:29:47 -0300

    vistaPiezas

--- Final/Final/Form5.Designer.cs
+++ Final/Final/Form5.Designer.cs
@@ -29,32 +29,176 @@
         private void InitializeComponent()
         {
             this.btnVolver = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNroPieza = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.dgvPiezas = new System.Windows.Forms.DataGridView();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPiezas)).BeginInit();
             this.SuspendLayout();
             // 
             // btnVolver
             // 
-            this.btnVolver.Location = new System.Drawing.Point(670, 395);
+            this.btnVolver.Location = new System.Drawing.Point(1787, 942);
+            this.btnVolver.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.btnVolver.Name = "btnVolver";
-            this.btnVolver.Size = new System.Drawing.Size(107, 43);
+            this.btnVolver.Size = new System.Drawing.Size(285, 103);
             this.btnVolver.TabIndex = 0;
             this.btnVolver.Text = "Volver";
             this.btnVolver.UseVisualStyleBackColor = true;
             this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Numero de pieza";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 324);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Descripcion";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(134, 532);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 32);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Stock";
+            // 
+            // txtNroPieza
+            // 
+            this.txtNroPieza.Location = new System.Drawing.Point(140, 207);
+            this.txtNroPieza.Name = "txtNroPieza";
+            this.txtNroPieza.Size = new System.Drawing.Size(223, 38);
+            this.txtNroPieza.TabIndex = 4;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(140, 414);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(223, 38);
+            this.txtDescripcion.TabIndex = 5;
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(140, 626);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(223, 38);
+            this.txtStock.TabIndex = 6;
+            // 
+            // dgvPiezas
+            // 
+            this.dgvPiezas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPiezas.Location = new System.Drawing.Point(717, 116);
+            this.dgvPiezas.Name = "dgvPiezas";
+            this.dgvPiezas.RowHeadersWidth = 102;
+            this.dgvPiezas.RowTemplate.Height = 40;
+            this.dgvPiezas.Size = new System.Drawing.Size(1146, 548);
+            this.dgvPiezas.TabIndex = 7;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(140, 942);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(253, 103);
+            this.btnEnviar.TabIndex = 8;
+            this.btnEnviar.Text = "Enviar Pieza";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(717, 701);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 70);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Alta";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(916, 701);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(193, 70);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Baja";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1115, 701);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(193, 70);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Modificar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1314, 701);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(193, 70);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Listar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form5
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(2133, 1073);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.dgvPiezas);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtNroPieza);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnVolver);
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "Form5";
             this.Text = "Stock Piezas";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPiezas)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNroPieza;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.DataGridView dgvPiezas;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
